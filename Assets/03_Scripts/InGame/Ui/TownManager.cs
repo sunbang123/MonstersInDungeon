53172c85586eac667c0b83742c86eac48cebda4a
yaruad <skun0709@gmail.com>
2025-11-25 01:57:33 +0900

    구역 입장 텍스트 구현

---
+++ Assets/03_Scripts/InGame/Ui/TownManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TownManager : MonoBehaviour
+{
+
+    public TMP_Text townText;
+    private float fadeDuration = 1f; //페이드 인아웃 시간
+    private float showDuration = 1.5f; //유지 시간
+
+
+    private Coroutine fadeRoutine;
+
+
+    void Start()
+    {
+        Color c = townText.color;
+        c.a = 0;
+        townText.color = c;
+    }
+
+    public void SetTownName(string townName)
+    {
+
+        townText.text = townName;
+
+        if (fadeRoutine != null)
+            StopCoroutine(fadeRoutine);
+
+        fadeRoutine = StartCoroutine(FadeText());
+    }
+
+    IEnumerator FadeText()
+    {
+        Color c = townText.color;
+
+        // Fade In
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(0f, 1f, t / fadeDuration);
+            townText.color = c;
+            yield return null;
+        }
+        c.a = 1f;
+        townText.color = c;
+
+        // 유지 시간
+        yield return new WaitForSeconds(showDuration);
+
+        // Fade Out
+        t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(1f, 0f, t / fadeDuration);
+            townText.color = c;
+            yield return null;
+        }
+        c.a = 0f;
+        townText.color = c;
+    }
+}
