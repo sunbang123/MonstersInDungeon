ea6f1357e70f5a10170e9967868d6954897bcf6a
yaruad <skun0709@gmail.com>
2025-12-03 03:50:29 +0900

    인벤토리 UI 구현

---
+++ Assets/03_Scripts/InGame/Ui/ItemSlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData item;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
